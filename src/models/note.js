50edb24f4fe3c7e2e330ef3ef4a5f8cc35aa700c
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2026-06-10 23:15:43 +0300

    fix autochck

--- src/models/note.js
+++ src/models/note.js
@@ -29,4 +29,6 @@ const noteSchema = new Schema(
   },
 );
 
+noteSchema.index({ tag: 1 });
+
 export const Note = model('Note', noteSchema);
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -5,7 +5,7 @@ import { TAGS } from '../constants/tags.js';
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
-    page: Joi.number().min(1).default(1),
+    page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow(''),
@@ -35,7 +35,7 @@ export const updateNoteSchema = {
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
+    title: Joi.string().min(1),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }).min(1),
