20b6a6f147d59ed832ee68f9bc90396fda33b745
Viktor Hnatiuk <viktorgnatiuk136@gmail.com>
2026-06-26 19:18:22 +0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { User } from '../models/user';
+import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const updateUserAvatar = async (req, res) => {
